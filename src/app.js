3b01ea40bcbcb6bc51e655339bca88d65a5a98dc
Mahathi Amencherla <mahathiamencherla15@gmail.com>
2020-07-03 15:09:22 +0530

    bouler plate

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+const jsx = (
+  <div>
+    <p>Boiler plate</p>
+  </div>
+);
+
+
+
+ReactDOM.render(jsx, document.getElementById('app'))
