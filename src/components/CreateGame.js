e1c605cecb0007c52c8b70c693039da48446705b
Mahathi Amencherla <mahathiamencherla15@gmail.com>
2020-07-03 21:56:12 +0530

    modal routes

--- src/components/CreateGame.js
+++ src/components/CreateGame.js
@@ -1,18 +1,52 @@
 import React from 'react';
+import TeamsModal from './TeamsModal';
+import MultiModal from './MultiModal';
+import CompModal from './CompModal';
 
-const CreateGame = ( ) => (
-  <div>
-    <p>Game type</p>
-    <p>Rules</p>
-    <div>
-      <p>Teams</p>
-      <a href="/rules_teams">Teams</a>
-      <p>Multiplayer</p>
-      <a href="/rules_multiplayer">Multiplayer</a>
-      <p>Computer</p>
-      <a href="/rules_computer">Computer</a>
+export default class CreateGame extends React.Component {
+  state = {
+    selectedOption: undefined
+  }
+
+  handleSelectedOption = () => {
+    this.setState(() => ({ selectedOption: undefined}))
+}
+teamsGame = () => {
+  this.setState(() => ({
+    selectedOption: "teams"
+}))
+}
+multiGame = () => {
+  this.setState(() => ({
+    selectedOption: "multi"
+}))
+}
+compGame = () => {
+  this.setState(() => ({
+    selectedOption: "comp"
+}))
+}
+  render() {
+    return (
+      <div>
+      <p>Game type</p>
+      <p>Rules</p>
+      <div>
+        <button onClick={this.teamsGame}>Teams</button>
+        <a href="/rules_teams">Teams</a>
+        <button onClick={this.multiGame}>Multiplayer</button>
+        <a href="/rules_multiplayer">Multiplayer</a>
+        <button onClick={this.compGame}>Computer</button>
+        <a href="/rules_computer">Computer</a>
+      </div>
+      {this.state.selectedOption === 'teams' && <TeamsModal selectedOption={this.state.selectedOption}
+            handleSelectedOption={this.handleSelectedOption}/>}
+      {this.state.selectedOption === 'multi' && <MultiModal selectedOption={this.state.selectedOption}
+            handleSelectedOption={this.handleSelectedOption}/>}   
+      {this.state.selectedOption === 'comp' && <CompModal selectedOption={this.state.selectedOption}
+            handleSelectedOption={this.handleSelectedOption}/>}         
     </div>
-  </div>
-)
-
-export default CreateGame;
+    )
+  }
+ 
+}
---
+++ src/components/TeamsModal.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Modal from 'react-modal';
+
+const TeamsModal = (props) => (
+    <Modal
+    isOpen={!!props.selectedOption}
+    onRequestClose={props.handleSelectedOption}
+    contentLabel="Teams"
+    closeTimeoutMS={200}
+    ariaHideApp={false}
+    className="modal">
+        <h3 className="modal__title">Teams</h3>
+        {/* {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>} */}
+        <select>
+          <option>Easy - 4 words</option>
+          <option>Medium - 5 words</option>
+          <option>Hard - 6 words</option>
+        </select>
+        <button className="button" onClick={props.handleSelectedOption}>Start Game</button>
+    </Modal>
+)
+
+export default TeamsModal;
