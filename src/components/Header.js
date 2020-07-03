fa0cb68dc7b929855a6c1b904e609009ab4f33c5
Mahathi Amencherla <mahathiamencherla15@gmail.com>
2020-07-04 04:36:30 +0530

    styling header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,8 @@ import { NavLink } from 'react-router-dom';
 const Header = () => (
     <header className="header-container">
         <h1 className="header__title">COWS & BULLS</h1>
-        <NavLink to="/" activeClassName="is-active" className="a-border" exact={true}>Create Game</NavLink>
-        <NavLink to="/join" activeClassName="is-active">Join Game</NavLink><br></br><br></br>
+        <NavLink to="/" activeClassName="is-active-1" className="a-border" exact={true}>Create Game</NavLink>
+        <NavLink to="/join" activeClassName="is-active-2" className="a-border-2">Join Game</NavLink><br></br><br></br>
     </header>
 );
 
