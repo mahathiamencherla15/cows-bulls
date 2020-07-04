14b34c898e01e041a7b0a5db5d98b39aac08612f
Mahathi Amencherla <mahathiamencherla15@gmail.com>
2020-07-04 09:13:01 +0530

    Style modifications to join and create

--- src/components/JoinGame.js
+++ src/components/JoinGame.js
@@ -5,9 +5,9 @@ const JoinGame = ( ) => (
     <div className="join-box">
     <h2 className="join-box__title">Hello!</h2>
     <form className="join-form">
-    <input type="text" name="username" placeholder="Enter your name" required/>
+    <input type="text" name="username" placeholder="Enter name" required/>
     <input type="text" name="room" placeholder="Enter room ID" required/>
-    <button>Join!</button>
+    <button>Join</button>
     </form>
   </div>
   </div>
